2d964d75eae73dac01df4b682659706af11da058
stecorte <ste.corte.dev+github7593@gmail.com>
2025-03-01 18:59:39 +0100

    Manage 2 letters system locale

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -7,17 +7,27 @@ export default getRequestConfig(async () => {
 
     let locale: string = defaultLocale
 
-    // Read locale from cookies
-    const cookieLocale: string | undefined = (await cookies()).get('x-locale')?.value
-    if (locales.includes(cookieLocale || 'zz-ZZ')) {
-        locale = cookieLocale || defaultLocale
-    }
+    // 1. Read locale from system preferences
+    let browserLocale: string | undefined = navigator.language
+    if (typeof browserLocale === "string") {
 
-    const browserLocale: string | undefined = navigator.language
-    if (typeof browserLocale === 'string') {
+        if (browserLocale.length == 2) {
+            if (browserLocale.toLowerCase() === "it") {
+                browserLocale = "it-IT"
+            } else if (browserLocale.toLowerCase() === "en") {
+                browserLocale = "en-GB"
+            }
+        }
+
         if (locales.includes(browserLocale)) {
             locale = browserLocale
         }
+    }
+
+    // 2. Override locale if set in cookie
+    const cookieLocale: string | undefined = (await cookies()).get("x-locale")?.value
+    if (locales.includes(cookieLocale || "zz-ZZ")) {
+        locale = cookieLocale || defaultLocale
     }
 
     return {
